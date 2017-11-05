90b9f0c2393072993992c0dd8c2a5d13ceab16fe
Hackathon <hackathon@e1z4r10p9.42.us.org>
2017-11-05 01:21:11 -0800

    profile

---
+++ splitplease/components/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, AppRegistry, Text} from 'react-native';
+
+export default class Profile extends React.Component {
+	render(){
+		return(
+			<View>
+				<Text>Hello world!</Text>
+			</View>
+		)
+	}
+}
